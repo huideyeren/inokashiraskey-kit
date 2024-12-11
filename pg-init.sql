4f2b44333f536d11265e3afe3cdc5d0362bc1dba
Iosif Takakura <takakura.yusuke@gmail.com>
2024-12-11 18:48:07 +0900

    update file

--- pg-init.sql
+++ pg-init.sql
@@ -1,3 +1,8 @@
-create role misskey login password 'misskey';  -- You may change it but it's mostly safe, because db is completely private.
-                                               -- When you change it make sure the db/pass value in default.yml is the same. 
+create role misskey login password 'misskey';
+-- You may change it but it's mostly safe, because db is completely private.
+-- When you change it make sure the db/pass value in default.yml is the same. 
 create database misskey owner misskey;
+
+-- mcaptcha settings
+create role mcaptcha login password 'mcaptcha';
+create database mcaptcha owner mcaptcha;
